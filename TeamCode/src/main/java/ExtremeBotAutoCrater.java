ab3e466465b25bfd7e43a36b417639b7fbd5d6b6
Arjun Rajagopala <arjun.rajagopala@gmail.com>
2018-11-28 20:25:44 -0500

    this won't work

--- TeamCode/src/main/java/ExtremeBotAutoCrater.java
+++ TeamCode/src/main/java/ExtremeBotAutoCrater.java
@@ -28,7 +28,7 @@ public class ExtremeBotAutoCrater extends LinearOpMode
     // Measured distance between minerals
     private static double distanceBetweenMineralsInches = 10.0;
 
-    /*s
+    /*
      * IMPORTANT: You need to obtain your own license key to use Vuforia. The string below with which
      * 'parameters.vuforiaLicenseKey' is initialized is for illustration only, and will not function.
      * A Vuforia 'Development' license key, can be obtained free of charge from the Vuforia developer
