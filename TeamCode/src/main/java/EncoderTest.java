09cd9b1fdda651ad4d321054f241a5b42350193f
Arjun Rajagopala <arjun.rajagopala@gmail.com>
2019-02-02 17:45:36 -0500

    hey there

--- TeamCode/src/main/java/EncoderTest.java
+++ TeamCode/src/main/java/EncoderTest.java
@@ -1,3 +1,4 @@
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
@@ -6,9 +7,20 @@ public class EncoderTest extends LinearOpMode {
     Robot robot = new Robot();
     @Override
     public void runOpMode() {
-        robot.init(hardwareMap, this);
-        waitForStart();
-        robot.encoderDriveForward(.2, 5, 20000);
+        try {
+            robot.init(hardwareMap, this);
+            waitForStart();
+            robot.leftFrontMotor.setPower(.2);
+            robot.rightFrontMotor.setPower(-.2);
+            while(opModeIsActive() && robot.leftFrontMotor.getCurrentPosition() <= 900000000) {
+                telemetry.addData("current encoder position", robot.leftFrontMotor.getCurrentPosition());
+                telemetry.update();
+            }
+            //robot.encoderDriveForward(.2, 5, 20000);
 
+        } catch (Exception ex) {
+            telemetry.addData("StackTrace", ex.getStackTrace());
+            telemetry.update();
+        }
     }
 }
